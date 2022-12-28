2609504adacc456ad65cd77a14aab5ce4e852ea0
sin0713 <narisin1124@gmail.com>
2022-12-28 16:34:39 +0900

    [Add]First Commit

---
+++ flutter_app/lib/main.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  final title = 'Flutter サンプル';
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(
+        title: this.title
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({required this.title}): super();
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String _message = 'Hello!';
+
+  void _setMessage() {
+    setState(() {
+      _message = 'タップしました！';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Text(
+        _message,
+        style: const TextStyle(fontSize: 32.0)
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _setMessage,
+          tooltip: 'set message.',
+          child: const Icon(Icons.star),
+        ),
+      );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
