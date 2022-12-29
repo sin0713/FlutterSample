cdded2ab97dee5b1e4a66c50c475a41f566f8710
sin0713 <narisin1124@gmail.com>
2022-12-29 20:38:35 +0900

    [Add]Navigator

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -18,115 +18,82 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xffe91e63),
         canvasColor: const Color(0xfffafafa),
       ),
-      home: MyHomePage(
-        title: this.title
+      home:FirstScreen()
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text('Home Screen',
+          style: const TextStyle(fontSize: 32.0)),
+        )
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        items: const <BottomNavigationBarItem>[
+           BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(Icons.home, size: 32),
+          ),
+           BottomNavigationBarItem(
+             label: 'next',
+             icon: Icon(Icons.navigate_next, size: 32),
+          )
+        ],
+        onTap: (int value) {
+          if (value == 1) {
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondScreen())            );
+          }
+        },
       ),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({required this.title}): super();
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class Data {
-  int _price;
-  String _name;
-  Data(this._name, this._price): super();
-
-  @override
-  String toString() {
-    return _name + ':' + _price.toString() + '円';
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  static var _message = 'ok';
-  static var _index = 0;
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-        ),
-        body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Padding(padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    _message,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      fontSize: 32.0,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"),
-                  ),
-                ),
-
-                ListView(
-                  shrinkWrap: true,
-                  padding: const EdgeInsets.all(20.0),
-                  children:  <Widget>[
-                    ListTile(
-                      leading: const Icon(Icons.android, size: 32),
-                      title: const Text(
-                          'first item',
-                          style: TextStyle(fontSize: 28)),
-                      selected: _index == 1,
-                      onTap: () {
-                        _index = 1;
-                        tapTile();
-                      }
-                    ),
-                    ListTile(
-                      leading: const Icon(Icons.favorite, size: 32),
-                      title: const Text(
-                          'second item',
-                          style: TextStyle(fontSize: 28)),
-                      selected: _index == 2,
-                      onTap: () {
-                        _index = 2;
-                        tapTile();
-                      }
-                    ),
-                   ListTile(
-                      leading: const Icon(Icons.home, size: 32),
-                      title: const Text(
-                          'third item',
-                          style: TextStyle(fontSize: 28)),
-                      selected: _index == 3,
-                      onTap: () {
-                        _index = 3;
-                        tapTile();
-                      }
-                    )
-                  ],
-                )
-              ],
-            ),
+      appBar: AppBar(
+        title: const Text('Next'),
+      ),
+      body: Center(
+        child: Container(
+          child: const Text('Home Screen',
+          style: const TextStyle(fontSize: 32.0)),
         )
-      );
-  }
-  void tapTile() {
-    setState(() {
-      _message = 'you tapped: No, $_index';
-    });
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        items: const <BottomNavigationBarItem>[
+           BottomNavigationBarItem(
+            label: 'prev',
+            icon: Icon(Icons.navigate_before, size: 32),
+          ),
+           BottomNavigationBarItem(
+             label: '?',
+             icon: Icon(Icons.android, size: 32),
+          )
+        ],
+        onTap: (int value) {
+          if (value == 0) {
+            Navigator.pop(context);
+          }
+        },
+      ),
+    );
   }
 }
-
-enum RadioType {
-  A, B
-}
-
 
 
 
