5cd5ca9f0bc3380d79ce96072000251621036075
sin0713 <narisin1124@gmail.com>
2022-12-29 19:16:21 +0900

    [Add]AppBar

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -46,7 +46,9 @@ class Data {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  static var _message = '';
+  static var _message = 'ok';
+  static var _stars = '☆☆☆☆☆';
+  static var _star = 0;
   static var _groupValue = RadioType.A;
 
   void checkChanged(RadioType? val) {
@@ -85,11 +87,52 @@ class _MyHomePageState extends State<MyHomePage> {
     ).then((value) => resultAlert(value));
   }
 
+  void buttonPressedA() {
+    _message = 'tap "android".';
+    _star++;
+    update();
+  }
+  void buttonPressedB() {
+    _message =  'tap "favorite".';
+    _star--;
+    update();
+  }
+  void update() {
+    _star = _star < 0 ? 0 : _star > 5 ? 5 : _star;
+    setState(() {
+      _stars = '★★★★★☆☆☆☆☆'.substring(5 - _star, 5 - _star + 5);
+      _message = _message + '[$_star]';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
+          leading: BackButton(color: Colors.white),
+          actions: <Widget>[
+            IconButton(
+                onPressed: buttonPressedA,
+                tooltip: 'add star....',
+                icon: Icon(Icons.android)
+            ),
+            IconButton(
+                onPressed: buttonPressedB,
+                tooltip: 'subtract star....',
+                icon: Icon(Icons.favorite)
+            ),
+          ],
+          bottom: PreferredSize(
+            preferredSize: const Size.fromHeight(50.0),
+            child: Center(
+              child: Text(_stars,
+              style: const TextStyle(
+                fontSize: 22.0,
+                color: Colors.white),
+              )
+            )
+          )
         ),
         body: Center(
           child: Column(
@@ -106,23 +149,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       fontWeight: FontWeight.w400,
                       fontFamily: "Roboto"),
                   ),
-                ),
-
-                const Padding(padding: EdgeInsets.all(10.0)),
-
-                Padding(padding: EdgeInsets.all(20.0),
-                child: ElevatedButton(
-                  onPressed: buttonPressed,
-                  child: const Text(
-                    "tap me!",
-                    style: TextStyle(
-                      fontSize: 32.0,
-                      color: Color(0xff000000),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                ),)
+                )
               ],
             ),
         )
