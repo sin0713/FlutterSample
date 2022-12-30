a879f607231f735011a57edf3a47c76e13e3e985
sin0713 <narisin1124@gmail.com>
2022-12-30 09:53:58 +0900

    TabBar

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -18,131 +18,66 @@ class MyApp extends StatelessWidget {
         primaryColor: const Color(0xffe91e63),
         canvasColor: const Color(0xfffafafa),
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => FirstScreen(),
-        '/second': (context) => SecondScreen('Second'),
-        '/third': (context) => ThirdScreen('Third')
-      },
+      home: FirstScreen(),
     );
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({Key? key}) : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: Center(
-        child: Container(
-          child: const Text('Home Screen',
-          style: TextStyle(fontSize: 32.0)),
-        )
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 1,
-        items: const <BottomNavigationBarItem>[
-           BottomNavigationBarItem(
-            label: 'Home',
-            icon: Icon(Icons.home, size: 32),
-          ),
-           BottomNavigationBarItem(
-             label: 'next',
-             icon: Icon(Icons.navigate_next, size: 32),
-          )
-        ],
-        onTap: (int value) {
-          if (value == 1) {
-            Navigator.pushNamed(context, '/second');
-          }
-        },
-      ),
-    );
-  }
+  _FirstScreenState createState() => _FirstScreenState();
 }
 
-class SecondScreen extends StatelessWidget {
-  final String _value;
-  const SecondScreen(this._value, {super.key});
+
+class _FirstScreenState extends State<FirstScreen>
+    with SingleTickerProviderStateMixin {
+
+  static const List<Tab> tabs = <Tab>[
+    Tab(text: 'One'),
+    Tab(text: 'Two'),
+    Tab(text: 'Three'),
+  ];
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: tabs.length, vsync: this);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Next'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: tabs,
+        ),
       ),
-      body: Center(
-        child: Container(
-          child:  Text('$_value Screen',
-          style: const TextStyle(fontSize: 32.0)),
-        )
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: const <BottomNavigationBarItem>[
-           BottomNavigationBarItem(
-            label: 'prev',
-            icon: Icon(Icons.navigate_before, size: 32),
-          ),
-           BottomNavigationBarItem(
-             label: '?',
-             icon: Icon(Icons.android, size: 32),
-          )
-        ],
-        onTap: (int value) {
-          if (value == 0) {
-            Navigator.pop(context);
-          }
-          if (value == 1) {
-            Navigator.pushNamed(context, '/third');
-          }
-        },
+      body: TabBarView(
+        controller: _tabController,
+        children: tabs.map((Tab tab) {
+          return  createTab(tab);
+        }).toList(),
+      )
+    );
+  }
+
+  Widget createTab(Tab tab) {
+    return Center(
+      child: Text(
+        'This is "${tab.text}" Tab.',
+        style: const TextStyle(
+        fontSize: 32.0,
+        color: Colors.blue)
       ),
     );
   }
 }
-
-
-class ThirdScreen extends StatelessWidget {
-  final String _value;
-  const ThirdScreen(this._value, {super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Next'),
-      ),
-      body: Center(
-        child: Container(
-          child:  Text('$_value Screen',
-          style: const TextStyle(fontSize: 32.0)),
-        )
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        items: const <BottomNavigationBarItem>[
-           BottomNavigationBarItem(
-            label: 'prev',
-            icon: Icon(Icons.navigate_before, size: 32),
-          ),
-           BottomNavigationBarItem(
-             label: '?',
-             icon: Icon(Icons.android, size: 32),
-          )
-        ],
-        onTap: (int value) {
-          if (value == 0) {
-            Navigator.pop(context);
-          }
-        },
-      ),
-    );
-  }
-}
-
 
 
 
